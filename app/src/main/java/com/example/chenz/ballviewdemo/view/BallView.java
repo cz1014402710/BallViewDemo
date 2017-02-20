f55bd02db10efb410c1ed449c4b8dc1aa4329047
cz1014402710 <937346702@qq.com>
2017-02-20 14:51:49 +0800

    12

---
+++ app/src/main/java/com/example/chenz/ballviewdemo/view/BallView.java
@@ -0,0 +1,23 @@
+package com.example.chenz.ballviewdemo.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by chenz on 2016/12/13.
+ */
+public class BallView extends View {
+    public BallView(Context context) {
+        super(context);
+    }
+
+    public BallView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public BallView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+}
